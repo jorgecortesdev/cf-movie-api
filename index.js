71eb0fefd32ee8efecde626304bc408208242713
Jorge Cortes <jorge.cortes@gmail.com>
2021-04-02 21:05:58 -0700

    Added end-points and documentation

--- index.js
+++ index.js
@@ -24,8 +24,52 @@ app.get('/', (req, res) => {
     res.send('Welcome to my app!');
 });
 
+/**
+ * Movies routes
+ */
 app.get('/movies', (req, res) => {
     res.json(movies);
+});
+
+app.get('/movies/:title', (req, res) => {
+    res.send(`Successful GET request returning movie "${req.params.title}."`)
+});
+
+/**
+ * Users routes
+ */
+app.post('/users/add', (req, res) => {
+    res.send('Successful POST request adding user.');
+});
+
+app.delete('/users/1', (req, res) => {
+    res.send('Successful DELETE request user removed.');
+});
+
+app.put('/users/1', (req, res) => {
+    res.send('Successful PUT request user updated.');
+});
+
+app.post('/users/:id/movies/:id/lists/:id/add', (req, res) => {
+    res.send('Successful POST request movie added.');
+});
+
+app.delete('/users/:id/movies/:id/lists/:id', (req, res) => {
+    res.send('Successful DELETE request movie removed.');
+});
+
+/**
+ * Genres
+ */
+app.get('/genres/:name', (req, res) => {
+    res.send(`Successful GET request returning ${req.params.name}.`);
+});
+
+/**
+ * Directors
+ */
+app.get('/directors/:name', (req, res) => {
+    res.send(`Successful GET request returning ${req.params.name}.`);
 });
 
 app.use((err, req, res, next) => {
