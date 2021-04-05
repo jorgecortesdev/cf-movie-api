83a5ca2e404f4105efcd80ba6c0b629635b486fa
Jorge Cortes <jorge.cortes@gmail.com>
2021-04-04 19:24:29 -0700

    All domains are allowed to make requests

--- index.js
+++ index.js
@@ -14,19 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan('common'));
 app.use(express.static('public'));
-
-let allowedOrigins = ['http://localhost:8080'];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            let message = `The CORS policy for this applicaction doesn't allow access from origin ${origin}`;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+app.use(cors());
 
 let auth = require('./auth')(app);
 
